e93840b080bcdcdf210ee48164a3b056f97dc77a
Carvalho001 <erlangleidson@hotmail.com>
2021-06-25 04:33:32 -0300

    atualização contendo complemento dos exercćios do dia 24/06/2021 sobre callback e funções de array.

--- semana2/funcoes-de-array/index.js
+++ semana2/funcoes-de-array/index.js
@@ -75,38 +75,64 @@
 //  ] 
 
 // const paraDonosDePoodle = pets.filter((poodle)=>{
-// if(poodle.raca === "Poodle"){
-//     console.log(`Você ganhou um cupom de desconto de
-// 10% para tosar o/a ${poodle.nome}`)
-//     return true
-// }else  {
-//     return false
+// return poodle.raca === "Poodle"
+// })
 
-//       } 
-//  })
-// console.log(paraDonosDePoodle)
+// const descontoPooodle = paraDonosDePoodle.map((poodle)=>{
+// return  `Você ganhou um cupom de desconto de 10% para tosar o/a ${poodle.nome}!`
+// })
+// console.log(descontoPooodle)
 
-2.
-const produtos = [
-    { nome: "Alface Lavada", categoria: "Hortifruti", preco: 2.5 },
-    { nome: "Guaraná 2l", categoria: "Bebidas", preco: 7.8 },
-    { nome: "Veja Multiuso", categoria: "Limpeza", preco: 12.6 },
-    { nome: "Dúzia de Banana", categoria: "Hortifruti", preco: 5.7 },
-    { nome: "Leite", categoria: "Bebidas", preco: 2.99 },
-    { nome: "Cândida", categoria: "Limpeza", preco: 3.30 },
-    { nome: "Detergente Ypê", categoria: "Limpeza", preco: 2.2 },
-    { nome: "Vinho Tinto", categoria: "Bebidas", preco: 55 },
-    { nome: "Berinjela kg", categoria: "Hortifruti", preco: 8.99 },
-    { nome: "Sabão em Pó Ypê", categoria: "Limpeza", preco: 10.80 }
- ]
 
-//a)
-const apeanasNomes = produtos.map((elemento) =>{
-    return elemento.nome
-})
-console.log(apeanasNomes)
+// 2.
+// const produtos = [
+//     { nome: "Alface Lavada", categoria: "Hortifruti", preco: 2.5 },
+//     { nome: "Guaraná 2l", categoria: "Bebidas", preco: 7.8 },
+//     { nome: "Veja Multiuso", categoria: "Limpeza", preco: 12.6 },
+//     { nome: "Dúzia de Banana", categoria: "Hortifruti", preco: 5.7 },
+//     { nome: "Leite", categoria: "Bebidas", preco: 2.99 },
+//     { nome: "Cândida", categoria: "Limpeza", preco: 3.30 },
+//     { nome: "Detergente Ypê", categoria: "Limpeza", preco: 2.2 },
+//     { nome: "Vinho Tinto", categoria: "Bebidas", preco: 55 },
+//     { nome: "Berinjela kg", categoria: "Hortifruti", preco: 8.99 },
+//     { nome: "Sabão em Pó Ypê", categoria: "Limpeza", preco: 10.80 }
+//  ]
+
+
+// //a)
+// const apeanasNomes = produtos.map((elemento) =>{
+//     return elemento.nome
+// })
+// console.log(apeanasNomes)
 
 //b)
+// const novoObjeto = produtos.map ((valor)=>{
+//     const calculoDoDesconto = (valor.preco* (5/100))
+//     return  {nome: valor.nome, preco:   (valor.preco - calculoDoDesconto) }
+// }) 
+// const resultadoFinal = novoObjeto.filter((fim)=>{
+// }) 
+// console.log(novoObjeto)
 
+//c)
+// const apenasBebibas = produtos.filter((bebidas)=>{
+//     return bebidas.categoria === "Bebidas"
+// })
+// console.log(apenasBebibas)
+
+//d)
+// const apenasYpe = produtos.filter((ype) =>{
+// return ype.nome.includes("Ypê")
+// })
+// console.log(apenasYpe)
+
+//e)
+// const apenasYpe = produtos.filter((ype) =>{
+//     return ype.nome.includes("Ypê")
+//     })
+// const imprimeYpeComMensagem = apenasYpe.map((item)=>{
+//     return `compre ${item.nome} por ${item.preco.toFixed(2)}`
+//     })
+//     console.log(imprimeYpeComMensagem)
 
 
