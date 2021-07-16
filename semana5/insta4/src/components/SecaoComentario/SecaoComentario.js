799350cdbfc8900cc6f8fc0a150266d9092fcc0d
Carvalho001 <erlangleidson@hotmail.com>
2021-07-16 10:25:35 -0300

    branch do dia 15/06/2021 contendo atualização do insta4 dessa vez utilizando conceitos da aula da quinta (15/06) qu foi renderização de listas.

--- semana5/insta4/src/components/SecaoComentario/SecaoComentario.js
+++ semana5/insta4/src/components/SecaoComentario/SecaoComentario.js
@@ -20,17 +20,18 @@ export class SecaoComentario extends Component {
 
 	onChangeComentario=(event) =>{
 		this.setState({
-		coment:event.target.coment
+		coment:event.target.value 
 		})
-		console.log(this.props.aoEnviar)
-	}
+	
+		}
 
 	render() {
+		console.log(this.state.coment )
 		return <CommentContainer>
 			<InputComentario
 				placeholder={'Comentário'}
-				value={this.state.coment}
-				onChange={this.onChangeComentario}
+				value={this.state.coment }
+				onChange={this.onChangeComentario }
 			/>
 			<button onClick={this.props.aoEnviar}>Enviar</button>
 		</CommentContainer>	
