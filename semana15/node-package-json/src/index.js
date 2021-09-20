122d517242e47e26d66b25ed135935e2c7fc11ae
erlan carvalho <erlangleidson@hotmail.com>
2021-09-20 15:31:38 -0300

    fiz o primeiro exercicio.

---
+++ semana15/node-package-json/src/index.js
@@ -0,0 +1,13 @@
+//1.
+
+// a) utilizamos process.argv para acessar argumentos passados aos nossos scripts 
+
+// b) & c)
+const inputName = process.argv[2]
+const inputAge = Number(process.argv[3]) 
+const ageAfter7years = Number(process.argv[3])  + 7
+console.log(`Olá ${inputName}! Você tem  ${inputAge} anos. Daqui a 7 anos você terá ${ageAfter7years } `)
+
+//2.
+
+
