e47a7f3236ddfff390569a2c1b3428d744b732e8
erlan carvalho <erlangleidson@hotmail.com>
2021-09-21 22:07:23 -0300

    Finalizei o terceiro exercicio.

--- semana15/introdTypescript/src/thirdExerc.ts
+++ semana15/introdTypescript/src/thirdExerc.ts
@@ -1,35 +1,16 @@
 // a)
 
-type thePost = [
+type post = 
 
   {
       autor:string,
       texto:string
-  },
-
-  {
-    autor:string,
-    texto:string
-},
-
-{
-    autor:string,
-    texto:string
-},
-
-{
-    autor:string,
-    texto:string
-},
-
-{
-    autor:string,
-    texto:string
-}
+  }
     
-];
+;
+type postsArray = post[]
 
-const posts :thePost = [
+const posts :postsArray = [
     {
       autor: "Alvo Dumbledore",
       texto: "Não vale a pena viver sonhando e se esquecer de viver"
@@ -52,4 +33,11 @@ const posts :thePost = [
     }
   ]
 
+// b)
+//R- As entradas são os parâmetros que serão as propriedades dos elementos dos objetos do array da variável posts. E as suas saídas será o nome do autor da postagem que se encontra nos objetos do array post.
 
+function buscarPostsPorAutor(posts:post[], autorInformado: string): post[]{
+    return posts.filter(
+      (post):boolean => { return post.autor === autorInformado}
+    )
+  }
