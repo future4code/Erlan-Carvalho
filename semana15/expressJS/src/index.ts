686eef11c4bc3adfc818fdc25fbe5d0bd5b3f6f1
erlan carvalho <erlangleidson@hotmail.com>
2021-09-22 16:46:02 -0300

    Corrigi o tsconfig.json, que estava com um erro na configuraçao 'outdir, e finalizie o exercicio 1 e 2 do projeto do dia.

--- semana15/expressJS/src/index.ts
+++ semana15/expressJS/src/index.ts
@@ -1,6 +1,6 @@
-import express from 'express';
+import express, {Request, Response} from 'express';
 import cors from 'cors';
-
+import {countries} from "./data"
 
 const app = express()
 
@@ -10,3 +10,38 @@ app.use(cors())
 app.listen(3003,  ()=>{
     console.log("Server is rruning in http://localhost:3003");
 });
+
+app.get("/countries", (req: Request, res: Response)=>{
+        const result = countries.map((nation)=>{
+        name: nation.name 
+        id: nation.id
+        })
+
+      res.status(200).send(result)
+})
+
+app.get("/countries/:id", (req:Request , res:Response) =>{
+  
+  try{
+      const result = countries.find((nation)=>{
+            nation.id === Number(req.params.id)
+      })
+
+      res.status(200).send(result)
+
+      if(!result){
+          throw new Error("Type a correct id ");
+          
+      }
+
+  }catch(error){
+      res.status(404).end()
+
+
+  }
+  
+})
+
+
+
+
