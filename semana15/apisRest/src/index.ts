48b43ba3c1001a6df7392cbb9a9ad1cfdbdf41e1
erlan carvalho <erlangleidson@hotmail.com>
2021-09-24 15:29:38 -0300

    Alteraçao nos documentos do exercicio

--- semana15/apisRest/src/index.ts
+++ semana15/apisRest/src/index.ts
@@ -1,5 +1,7 @@
-import express from "express";
+import express, {Response, Request} from "express";
 import cors from "cors";
+import { users } from "./data";
+
 
 const app = express()
 
@@ -10,3 +12,12 @@ app.listen(3003, ()=>{
     console.log("Server is runig at http://localhost:3003")
 });
 
+app.get("/users", (res:Response, req:Request)=>{
+
+try{
+
+}catch(error){
+    res 
+}
+
+})
