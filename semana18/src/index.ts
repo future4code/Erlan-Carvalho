f4f5f8308fd500a1b98962f54718a3fa985407d7
erlan carvalho <erlangleidson@hotmail.com>
2021-10-12 05:13:32 -0300

    Finalizei o exercicio 3

--- semana18/src/index.ts
+++ semana18/src/index.ts
@@ -14,7 +14,7 @@ class UserAccount {
    private balance: number = 0;
    private transactions: Transaction[] = [];
 
-   constructor(cpf: string,name: string,  age: number,) {
+   constructor(cpf: string, name: string, age: number,) {
       console.log("Chamando o construtor da classe UserAccount")
       this.cpf = cpf;
       this.name = name;
@@ -23,7 +23,7 @@ class UserAccount {
 
 }
 
-  const getUserAccount = new UserAccount ("255.025.888-44", "José", 30)
+const getUserAccount = new UserAccount("255.025.888-44", "José", 30)
 //R - A mensagem "Chamando o construtor da classe User" foi impressa no terminal apenas 1 vez. 
 
 // c) Criando funções que funcionem como seus getters. Ao utilizar essas funções fora do escopo da classe conseguimos acesso à propriedades privadas.
@@ -50,4 +50,14 @@ class Transaction {
 
 }
 const getTransaction = new Transaction("Transação financeira,", 50, "2021/10/10")
-console.log(getUserAccount, getTransaction )
+console.log(getUserAccount, getTransaction)
+
+//Exercício 3
+
+class Bank {
+   private account: UserAccount[];
+
+   constructor(account: UserAccount[]) {
+      this.account = account
+   }
+}
