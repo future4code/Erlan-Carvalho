e623fcc523f34261da032206086b4bd22468051a
erlan carvalho <erlangleidson@hotmail.com>
2021-10-18 22:29:16 -0300

    Criei um novo documento apenas para deixar a funçao connection, que esta diretamente ligada com o banco de dados.

--- semana19/aula55/src/services/SaveUser.ts
+++ semana19/aula55/src/services/SaveUser.ts
@@ -1,19 +1,7 @@
-import knex from "knex"
+import { connection } from "../data/connection";
 
 // respostas da 2ª letra c) 
 
-
-export const connection = knex({
-    client: "mysql",
-    connection: {
-        host: process.env.DB_HOST,
-        port: 3006,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASS,
-        database: process.env.DB_NAME
-    }
-
-})
 
 const userTableName = "toDoListUser";
 
